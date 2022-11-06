d79143c17a0f0166438e93b9d99651eb079ad59d
Ryan <sonnb@mageplaza.com>
2022-11-06 18:21:02 +0700

    --Ryan

--- react-app-ryan/src/index.js
+++ react-app-ryan/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import "@shopify/polaris/dist/styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
+ReactDOM.render(
+    <>
     <App />
-  </>
-);
+    </>,
+    document.getElementById("root")
+)
 
