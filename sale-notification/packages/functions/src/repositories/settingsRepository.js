97f53ee38959eb21d6c96e2ffed03dd8d0d09553
Ryan <sonnb@mageplaza.com>
2022-11-18 15:06:13 +0700

    --ryan

--- sale-notification/packages/functions/src/repositories/settingsRepository.js
+++ sale-notification/packages/functions/src/repositories/settingsRepository.js
@@ -1,4 +1,5 @@
 import {Firestore} from '@google-cloud/firestore';
+
 const firebase = require("firebase-admin");
 const firestore = new Firestore();
 
@@ -10,37 +11,56 @@ const firestore = new Firestore();
  */
 const settingsRef = firestore.collection('settings');
 
+/**
+ *
+ * @param shopID
+ * @returns {Promise<(*&{id})|null>}
+ */
 async function getSettings(shopID) {
-  const settingDocs = await settingsRef
-    .where('shopId', '==', shopID)
-    .limit(1)
-    .get();
-  if (settingDocs.empty) {
-    return null;
+  try {
+    const settingDocs = await settingsRef
+      .where('shopId', '==', shopID)
+      .limit(1)
+      .get();
+    if (settingDocs.empty) {
+      return null;
+    }
+
+    const settingDoc = settingDocs.docs[0];
+
+    return {
+      id: settingDoc.id,
+      ...settingDoc.data()
+    };
+  } catch (e) {
+    throw new Error('Error!')
   }
-
-  const settingDoc = settingDocs.docs[0];
-
-  return {
-    id: settingDoc.id,
-    ...settingDoc.data()
-  };
 }
 
+/**
+ *
+ * @param data
+ * @param shopID
+ * @returns {Promise<boolean>}
+ */
 async function saveSettings(data, shopID) {
-  const settingDocs = await settingsRef
-    .where('shopId', '==', shopID)
-    .limit(1)
-    .get();
-  //create new
-  if (settingDocs.empty) {
-    settingsRef.add({timestamp: firebase.firestore.FieldValue.serverTimestamp(),shopId: shopID, ...data});
-    return true;
-  }
+  try {
+    const settingDocs = await settingsRef
+      .where('shopId', '==', shopID)
+      .limit(1)
+      .get();
+    //create new
+    if (settingDocs.empty) {
+      settingsRef.add({timestamp: firebase.firestore.FieldValue.serverTimestamp(), shopId: shopID, ...data});
+      return true;
+    }
 
-  const settingDoc = settingDocs.docs[0];
-  settingsRef.doc(settingDoc.id).set(data);
-  return true;
+    const settingDoc = settingDocs.docs[0];
+    settingsRef.doc(settingDoc.id).set(data);
+    return true;
+  } catch (e) {
+    throw new Error('Error!')
+  }
 }
 
 module.exports = {
