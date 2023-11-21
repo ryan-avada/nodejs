d8cefe58bf77efb00025ae03f2948ac5752b09ae
Ryan <sonnb@mageplaza.com>
2023-11-22 02:14:24 +0700

    --Ryan

--- koajs-exercises/src/app.js
+++ koajs-exercises/src/app.js
@@ -2,8 +2,22 @@ const Koa = require('koa');
 const koaBody = require('koa-body');
 const routers = require('./routers/routers');
 const cors = require('@koa/cors');
+const render = require('koa-ejs');
+const path = require('path');
 
 const app = new Koa();
+
+render(app, {
+    root: path.join(__dirname, 'view'),
+    layout: 'template',
+    viewExt: 'html',
+    cache: false,
+    debug: true
+});
+
+app.use(async function (ctx) {
+    await ctx.render('template');
+});
 
 app.use(cors());
 app.use(koaBody());
